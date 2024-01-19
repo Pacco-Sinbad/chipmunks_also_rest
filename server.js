8facee40ae127891ac7b5c137b7fb09ed9732b7f
Mathias Fischer <mfischerf92@gmail.com>
2024-01-19 00:13:30 -0600

    added cors

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 const PORT = 8000
+const cors = require('cors')
+app.use (cors())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(express.static('public'))
